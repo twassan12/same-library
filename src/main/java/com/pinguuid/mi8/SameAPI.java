8864a9a22de62cf66a89fc583f2df853e1f27fc3
pinguuid <pinguuid@gmail.com>
2020-10-03 17:16:48 +0300

    renam init() method

--- src/main/java/com/pinguuid/mi8/SameAPI.java
+++ src/main/java/com/pinguuid/mi8/SameAPI.java
@@ -1,9 +1,9 @@
 package com.pinguuid.mi8;
 
 public interface SameAPI {
-  String init();
+  String initialize();
 
-  String init(Long when, String why);
+  String initialize(Long when, String why);
 
-  String init(Long when, String why, String how);
+  String initialize(Long when, String why, String how);
 }
--- src/main/java/com/pinguuid/mi8/SameImpl.java
+++ src/main/java/com/pinguuid/mi8/SameImpl.java
@@ -1,15 +1,15 @@
 package com.pinguuid.mi8;
 
 public class SameImpl implements SameAPI {
-  public String init() {
+  public String initialize() {
     return "nothing to say";
   }
 
-  public String init(Long when, String why) {
+  public String initialize(Long when, String why) {
     return "nothing to say at " + when + " because of " + why;
   }
 
-  public String init(Long when, String why, String how) {
+  public String initialize(Long when, String why, String how) {
     return "nothing to say " + how + " at " + when + " because of " + why;
   }
 }
